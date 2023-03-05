424e67032fc92823669608861c260db0c0955e8c
Pluto <1804643+Dani21@users.noreply.github.com>
2023-03-05 02:52:53 +0000

    Development refinements

--- start.sh
+++ start.sh
@@ -15,35 +15,50 @@ CONTAINER_NAME="supercharge-api"
 SCRIPT_DIR=`dirname $0`;
 DB_NAME="supercharge-db"
 DB_HOST="$DB_NAME:5432"
+CONN=false
 
 #
 # Network setup
 #
-docker network create supercharge-net
-RESP=`docker network connect supercharge-net $DB_NAME`
-if [[ $RESP == *"No such container"* ]]; then
-    echo $RESP
-    echo "Please create container from database folder first"
-    exit
+RESP=`docker network ls`
+if [[ $RESP != *supercharge-net* ]]; then
+    CONN=true
+    docker network create supercharge-net
 fi
 
-RESP=`docker ps`
-if [[ $RESP != *$DB_NAME* ]]; then
+LIST=`docker ps`
+if ! [[ $LIST =~ $DB_NAME($'\n'|$) ]]; then
+    CONN=true
     echo "Starting $DB_NAME..."
-    docker start $DB_NAME
+    RESP=`docker start $DB_NAME`
+
+    if [[ $RESP == *"No such container"* ]]; then
+        echo $RESP
+        echo "Please create container from database folder first"
+        exit
+    fi
 fi
+
+if [ "$CONN" = true ]; then
+    docker network connect supercharge-net $DB_NAME
+fi
+
 
 #
 # Build image
 #
 docker build --tag $IMAGE_TAG "$SCRIPT_DIR/"
 
+if [[ $LIST =~ $CONTAINER_NAME($'\n'|$) ]]; then
+    docker kill $CONTAINER_NAME
+fi
+
 #
 # Create and start the container
 #
 docker run --name $CONTAINER_NAME \
            --detach \
-           --env "JAVA_OPTS=-Ddb.url=jdbc:postgresql://$DB_HOST/postgres" \
+           --env "JAVA_OPTS=-Ddb.url=jdbc:postgresql://$DB_HOST/postgres -Dspring.profiles.active=development" \
            --rm \
            --network supercharge-net \
            --publish 8080:8080 \

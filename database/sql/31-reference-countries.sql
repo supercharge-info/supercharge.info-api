ab7b881cc22ab76cd4ac6558df7c523d4b877b05
Rovastar <rovastar@hotmail.com>
2024-12-24 02:36:48 +0000

    Add port type tpc/nacs for chinaUpdate 31-reference-countries.sql

--- database/sql/31-reference-countries.sql
+++ database/sql/31-reference-countries.sql
@@ -67,6 +67,6 @@ INSERT INTO country (country_id,"name",code,region_id,state_required,modified_da
 
 -- pre-populate valid plug types per country
 UPDATE country SET plugs_gbt = false WHERE name != 'China';
-UPDATE country SET plugs_tpc = false, plugs_nacs = false, plugs_ccs1 = false WHERE region_id IN (101, 102) AND name NOT IN ('Japan', 'South Korea', 'Taiwan');
+UPDATE country SET plugs_tpc = false, plugs_nacs = false, plugs_ccs1 = false WHERE region_id IN (101, 102) AND name NOT IN ('Japan', 'South Korea', 'Taiwan', 'China');
 UPDATE country SET plugs_type2 = false, plugs_ccs2 = false WHERE region_id = 100 OR name IN ('Japan', 'South Korea');
 UPDATE country SET plugs_ccs1 = false WHERE name IN ('Japan', 'Taiwan');
